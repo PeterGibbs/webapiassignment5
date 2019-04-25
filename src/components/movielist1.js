2235b53bfc642481558300309b8be91112706e8b
Peter Gibbs <pgibbs65@gmail.com>
2019-04-25 00:20:43 -0600

    attempting to fix stupid case sensitivity issue

--- src/components/movielist1.js
+++ src/components/movielist1.js
@@ -1,4 +1,4 @@
-git import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { fetchMovies } from '../actions/movieActions';
 import { setMovie } from '../actions/movieActions';
 import {connect} from "react-redux";
